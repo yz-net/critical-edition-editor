ed3fd8b8907c7420d0e6c652dabedb0c0968fd91
Jake Kara <jake@jakekara.com>
2020-11-17 13:07:15 -0500

    Fix multiple link replacement issue

--- src/components/Viewer/ContentBody/Block/Paragraph/index.tsx
+++ src/components/Viewer/ContentBody/Block/Paragraph/index.tsx
@@ -1,43 +1,58 @@
 import React, { useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import { ParagraphBlockData } from "../../../../../CriticalEditionData";
 import validParagraphBlockData from "../../../../../CriticalEditionData/validators/validParagraphBlockData";
-// import DebugLogger from "../../../utils/DebugLogger";
 import styles from "./Paragraph.module.css";
 import { ReactComponent as FootnoteIcon } from "../../../svg/footnote_icon.svg";
 import { renderToString } from "react-dom/server";
 import scrollToElementByID from "../../../../../utils/scrollToElementByID";
+import DebugLogger from "../../../../../utils/DebugLogger";
 
-// const logger = new DebugLogger("Paragraph: ");
+const logger = new DebugLogger("Paragraph: ");
 
 export function Paragraph(props: { data: ParagraphBlockData }) {
   let paragraphData: ParagraphBlockData;
   const ref = useRef<HTMLDivElement>(null);
-  const location = useLocation();
+  // const location = useLocation();
+
   useEffect(() => {
     if (ref.current) {
-      const links = ref.current.getElementsByTagName("a");
-      for (let i = 0; i < links.length; i++) {
-        const link = links[i];
+      const linkCollection = ref.current.getElementsByTagName("a");
 
+      // make a permanent list of links. since we will be replacing them
+      // they will disappear from linkCollection, making it impossible to
+      // both iterate through and modify the links. I know, right?
+      const linkReferences: Array<HTMLAnchorElement> = [];
+      for (let i = 0; i < linkCollection.length; i++) {
+        linkReferences.push(linkCollection[i]);
+      }
+
+      // now iterate through the permanent list of references
+      for (let i = 0; i < linkReferences.length; i++) {
+        const link = linkReferences[i];
+        if (!link) {
+          continue;
+        }
         const href = link.getAttribute("href") || "";
         if (href.indexOf("#") !== 0) {
-          return;
+          continue;
         }
 
         // TODO - add styling to make these links stand out better
+        // add a data attribute that can be used to highlight this
+        // icon when the paragraph is open/active
         link.classList.add(styles.FootnoteLink);
         const label = link.innerText.replace("[", "").replace("]", "");
         // link.innerHTML = `<span>
         //     ${renderToString(<FootnoteIcon />)}
         //   </span>`;
+        // logger.log("Fixing footnote: " + label);
 
         const newElement = document.createElement("button");
         newElement.classList.add(styles.FootnoteLink);
         newElement.innerHTML = `${renderToString(
           <FootnoteIcon />
         )} <sup>${label}</sup> `;
-        link.replaceWith(newElement);
 
         newElement.onclick = (e) => {
           const id = href.replace("#", "");
@@ -49,9 +64,12 @@ export function Paragraph(props: { data: ParagraphBlockData }) {
           //   e.preventDefault();
           // }
         };
+
+        ref.current.replaceChild(newElement, link);
+        // link.replaceWith(newElement);
       }
     }
-  }, [location.hash]);
+  }, [ref]);
 
   try {
     paragraphData = validParagraphBlockData(props.data);
