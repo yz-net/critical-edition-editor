0b29b2fde94c6a79f8a2fd2b37bfafbe49db4446
Jake Kara <jake@jakekara.com>
2021-02-09 14:17:07 -0500

    Change  download links to one-liner

--- src/components/Viewer/EssayPreamble/index.tsx
+++ src/components/Viewer/EssayPreamble/index.tsx
@@ -1,25 +1,24 @@
 import React from "react";
 import styles from "./EssayPreamble.module.css";
-import { ReactComponent as VideoIcon } from "../svg/video-icon.svg";
-import { ReactComponent as DocumentIcon } from "../svg/download_icon.svg";
+// import { ReactComponent as VideoIcon } from "../svg/video-icon.svg";
+// import { ReactComponent as DocumentIcon } from "../svg/download_icon.svg";
 
 export interface EssayPreambleProps {
   hvtID: string;
 }
 export default function EssayPreamble(props: EssayPreambleProps) {
   return (
-    <div className={styles.EssayPreamble}>
+    <div className={`sans-copy-ff ${styles.EssayPreamble}`}>
       <p>
-        <a className="teal-svg" href="https://google.com">
-          <VideoIcon />
-        </a>
-        View the testimony with annotated transcript.
-      </p>
-      <p>
-        <a href="https://google.com">
-          <DocumentIcon className="teal-svg" />
-        </a>
-        Download critical edition as a PDF.
+        {`HVT-${props.hvtID}`} |{" "}
+        <a className="teal-svg teal-font" href="https://google.com">
+          View the annotated testimony
+        </a>{" "}
+        or{" "}
+        <a className="teal-svg teal-font" href="https://google.com">
+          download a PDF
+        </a>{" "}
+        of the critical edition.
       </p>
     </div>
   );
