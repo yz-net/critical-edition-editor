254780b15ae7c7b31c0e1638ff2515d76e959ac7
Constantin Zach <constantin.zach@pm.me>
2024-02-06 11:59:09 +0100

    fixed some fonts, added more data

--- src/app/essay/[essayID]/page.tsx
+++ src/app/essay/[essayID]/page.tsx
@@ -1,87 +1,85 @@
-"use client"
+"use client";
 
-import { useParams } from "next/navigation"
+import { useParams } from "next/navigation";
 
 import LogoBar from "~/components/LogoBar";
 
-import config from "public/data/config.json" assert { type: "json" }
+import config from "public/data/config.json" assert { type: "json" };
 
-import styles from "./styles.module.scss"
-
-
+import styles from "./styles.module.scss";
 
 export default function essayPage() {
-    const params = useParams();
+  const params = useParams();
 
-    const essay = config.essays.find((essay) => essay.id === params.essayID);
+  const essay = config.essays.find((essay) => essay.id === params.essayID);
 
-    if(!essay) {
-        return <div>404</div>
-    }
+  if (!essay) {
+    return <div>404</div>;
+  }
 
-    const splashImage = (
-        <div
-          className={styles.SplashBackgroundImage}
-          style={{
-            backgroundImage: `url(${essay.posterPath})`,
-          }}
-        >
-          {' '}
-        </div>
-      );
+  const splashImage = (
+    <div
+      className={styles.SplashBackgroundImage}
+      style={{
+        backgroundImage: `url(${essay.posterPath})`,
+      }}
+    >
+      {" "}
+    </div>
+  );
 
-    const splashVideo = (
-        <video
-          poster={essay.posterPath}
-          playsInline
-          muted
-          loop
-          autoPlay
-          disablePictureInPicture
-          className={styles.SplashBackgroundVideo}
-        >
-          <source src={essay.videoPath} type="video/mp4" />
-        </video>
-      );
+  const splashVideo = (
+    <video
+      poster={essay.posterPath}
+      playsInline
+      muted
+      loop
+      autoPlay
+      disablePictureInPicture
+      className={styles.SplashBackgroundVideo}
+    >
+      <source src={essay.videoPath} type="video/mp4" />
+    </video>
+  );
 
-    const splashContent = essay.videoPath ? splashVideo : splashImage;
+  const splashContent = essay.videoPath ? splashVideo : splashImage;
 
-    return (
-        <div className={styles.Viewer}>
-            <div className={styles.LogoBarContainer}>
-                <LogoBar />
+  return (
+    <div className={`serif-copy-ff ${styles.Viewer}`}>
+      <div className={styles.LogoBarContainer}>
+        <LogoBar />
+      </div>
+
+      <div className={styles.PageContent}>
+        <header className={styles.SplashTitleContainer}>
+          {essay.videoPath ? <div className={styles.Gradient} /> : null}
+          <div className={styles.SplashBackgroundVideoContainer}>
+            {splashContent}
+          </div>
+          <div className={styles.SplashTitle}>
+            <div>
+              {/* <header> */}
+              {/* See example 5: https://www.w3.org/TR/html52/common-idioms-without-dedicated-elements.html#subheadings-subtitles-alternative-titles-and-taglines */}
+              <p className={`sans-title-ff my-[1em] ${styles.SuperTitle}`}>
+                {essay.supertitle}
+              </p>
+              <h1>{essay.title}</h1>
+              {/* </header> */}
             </div>
-
-            <div className={styles.PageContent}>
-          <header className={styles.SplashTitleContainer}>
-            {essay.videoPath ? <div className={styles.Gradient} /> : null}
-            <div className={styles.SplashBackgroundVideoContainer}>
-              {splashContent}
-            </div>
-            <div className={styles.SplashTitle}>
-              <div>
-                {/* <header> */}
-                {/* See example 5: https://www.w3.org/TR/html52/common-idioms-without-dedicated-elements.html#subheadings-subtitles-alternative-titles-and-taglines */}
-                <p className={`sans-title-ff ${styles.SuperTitle}`}>
-                  {essay.supertitle}
-                </p>
-                <h1>{essay.title}</h1>
-                {/* </header> */}
-              </div>
-              <div>
-                {/* <div className={styles.SplashMetaDivider}></div> */}
-                <div className={styles.SplashMeta}>
-                  by {essay.author}
-                  <div className={`${styles.Affiliation} sans-copy-ff`}>
-                    {essay.affiliation}
-                  </div>
+            <div>
+              {/* <div className={styles.SplashMetaDivider}></div> */}
+              <div className={styles.SplashMeta}>
+                by {essay.author}
+                <div className={`${styles.Affiliation} sans-copy-ff`}>
+                  {essay.affiliation}
                 </div>
               </div>
             </div>
-            <div className={styles.SplashTitleTail} />
-          </header>
+          </div>
+          <div className={styles.SplashTitleTail} />
+        </header>
 
-          {/* <EssayPreamble hvtID={hvtID} aviaryLink={essayAviaryLink} />
+        {/* <EssayPreamble hvtID={hvtID} aviaryLink={essayAviaryLink} />
 
           <div
             // style={{ top: Math.max(0, 200 - this.state.scrollPosition) }}
@@ -110,7 +108,7 @@ export default function essayPage() {
               parentOrgURL={config.projectData.parentOrganizationURL || ''}
             /> 
           </div>*/}
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import LogoBar from "~/components/LogoBar";
 import ImpactHeader from "~/components/ImpactHeader";
 
-import config from "public/data/config.json" assert { type: "json" }
+import config from "public/data/config.json" assert { type: "json" };
 
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 import EssayIndexItem from "~/components/EasyIndexItem";
-
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="serif-copy-ff">
       <LogoBar />
-       <ImpactHeader
+      <ImpactHeader
         caption="Photo: Steven H. and Marion L. Holocaust testimony (HVT-544), recorded in 1985."
         backgroundImageURL="/img/impact-header-background.jpg"
         title="Critical Editions"
@@ -19,12 +18,10 @@ export default function HomePage() {
       />
       <main className={styles.CenterColumn}>
         <div className={styles.IndexHeader}>
-          <p className="sans-copy-ff">The Critical Editions Series contextualizes Fortunoff Video testimonies in their historical time and place. Each testimony in the series was chosen by one of our visiting scholars. Each scholar then produced an introductory essay about the chosen testimony, along with an annotated transcript that provides additional insight and background information.</p>
+          <p className="sans-copy-ff">{config.projectData.introCopy}</p>
         </div>
         <nav aria-label="List of essays">
-          <ul
-            className={styles.ItemListContainer}
-          >
+          <ul className={styles.ItemListContainer}>
             {config.essays.map((essay) => {
               // const essay: EssayDataEntry = essays[essayID];
               if (!essay) {
@@ -34,7 +31,9 @@ export default function HomePage() {
               return (
                 <li key={essay.id} className={styles.IndexItemContainer}>
                   <EssayIndexItem
-                    showSupertitles={config.projectData.showSupertitlesOnIndexPage}
+                    showSupertitles={
+                      config.projectData.showSupertitlesOnIndexPage
+                    }
                     showBylines={config.projectData.showBylinesOnIndexPage}
                     textOnly={config.projectData.textOnlyIndexPage}
                     essay={essay}
