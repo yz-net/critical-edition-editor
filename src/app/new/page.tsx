9d106f9180b55c273f57c6ecfb14a0ed4ea0343e
Constantin Zach <constantin.zach@robotic-eyes.com>
2024-02-12 10:31:41 +0100

    persisting metadata in page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -12,20 +12,20 @@ import styles from "../essay/[essayID]/styles.module.scss";
 
 const MEDIA_PATH_PREFIX = "https://d12q9fe14kxf9b.cloudfront.net";
 
-interface Metadata {
-  title: string;
-  hvtID: string;
-  author: string;
-  affiliation: string;
-  publicationDate: string;
-}
-
 function getPosterPath(hvtID: string) {
   return `${MEDIA_PATH_PREFIX}/${hvtID}/poster.jpg`;
 }
 
 function getVideoPath(hvtID: string) {
   return `${MEDIA_PATH_PREFIX}/${hvtID}/background-loop-1280.mp4`;
+}
+
+interface Metadata {
+  title: string;
+  hvtID: string;
+  author: string;
+  affiliation: string;
+  publicationDate: string;
 }
 
 export default function NewPage() {
@@ -51,6 +51,127 @@ export default function NewPage() {
         <LogoBar />
       </div>
 
+      <div className="flex-shrink flex-grow-0 basis-full overflow-scroll">
+        <header className="relative z-[40] box-border h-[50vh] overflow-hidden">
+          <div className="absolute bottom-0 left-[calc(50%-400px)] right-[calc(50%-400px)] top-0 z-[100] bg-[linear-gradient(90deg,#000_10%,rgba(0,0,0,.1)49%,rgba(0,0,0,.1)51%,#000_90%)]" />
+          <div className="absolute inset-0 z-0 flex flex-col items-center justify-center overflow-hidden bg-black">
+            {data.hvtID && (
+              <video
+                className="h-full max-h-full w-full max-w-[800px] object-cover object-center"
+                playsInline
+                loop
+                autoPlay
+                disablePictureInPicture
+                ref={videoRef}
+                poster={getPosterPath(data.hvtID)}
+              >
+                <source src={getVideoPath(data.hvtID)} />
+              </video>
+            )}
+          </div>
+          <div className="relative z-[200] flex h-full flex-col justify-end bg-[rgba(0,0,0,.3)] px-[30px] pb-[30px] text-white drop-shadow-[0_0_9px_#000]">
+            <div className="absolute right-3 top-3 flex items-center gap-3">
+              <button
+                className={twMerge(
+                  "scale-100 rounded p-3 transition-[transform,colors] hover:scale-110",
+                  data.hvtID ? "bg-green-500" : "bg-neutral-500",
+                )}
+                onClick={() => {
+                  const newHvtID =
+                    window.prompt(
+                      "This should show metadata modal. hvtID:",
+                      data.hvtID,
+                    ) ?? "";
+                  setData((prev) => ({ ...prev, hvtID: newHvtID }));
+                }}
+                type="button"
+              >
+                <FiFilm />
+              </button>
+            </div>
+            <p className="sans-title-ff my-[1em] text-[13px] uppercase tracking-[.5px]">
+              introduction to the testimony of
+            </p>
+
+            <input
+              type="textarea"
+              name="Title"
+              placeholder="Title"
+              className="mb-[0.75em] h-[56px] w-full rounded bg-transparent p-0 text-[56px] focus:border-white focus:ring-0"
+              onChange={(newTitle) => {
+                setData((prev) => ({ ...prev, title: newTitle.target.value }));
+              }}
+            />
+            <div>
+              <div className="text-[24px]">
+                <div className="flex items-center gap-1">
+                  by
+                  <input
+                    type="textarea"
+                    name="Author"
+                    placeholder="Author"
+                    className="h-[45px] flex-1 rounded bg-transparent p-0 text-[24px] focus:border-white focus:ring-0"
+                    onChange={(newAuthor) => {
+                      setData((prev) => ({
+                        ...prev,
+                        author: newAuthor.target.value,
+                      }));
+                    }}
+                  />
+                </div>
+                <div className="font-helvetica text-[13px]">
+                  <input
+                    type="textarea"
+                    name="Affiliation"
+                    placeholder="Affiliation"
+                    className="h-[13px] w-full rounded bg-transparent p-0 text-[13px] focus:border-white focus:ring-0"
+                    onChange={(newAffiliation) => {
+                      setData((prev) => ({
+                        ...prev,
+                        affiliation: newAffiliation.target.value,
+                      }));
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="h-80 bg-white" />
+        </header>
+
+        <EssayPreamble hvtID={"test"} aviaryLink={"test"} />
+
+        <div
+          // style={{ top: Math.max(0, 200 - this.state.scrollPosition) }}
+          className={styles.ContentBodyContainer}
+        >
+          <main className={styles.ContentBodyContents}>
+            <Editor />
+          </main>
+
+          {/*
+          {callToAction && essayAviaryLink ? (
+            <div className={styles.CallToActionArea}>
+              <CallToAction posterURL={essayPosterPath} essay={essay} />
+            </div>
+          ) : null}
+
+          <Footer
+            orgName={config.projectData.organizationName || ""}
+            orgURL={config.projectData.homeLink || ""}
+            parentOrgName={config.projectData.parentOrganizationName || ""}
+            parentOrgURL={config.projectData.parentOrganizationURL || ""}
+          />
+          */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ModalDialog() {
+  return (
+    <>
       <button
         data-modal-target="authentication-modal"
         data-modal-toggle="authentication-modal"
@@ -180,105 +301,6 @@ export default function NewPage() {
           </div>
         </div>
       </div>
-
-      <div className="flex-shrink flex-grow-0 basis-full overflow-scroll">
-        <header className="relative z-[40] box-border h-[50vh] overflow-hidden">
-          <div className="absolute bottom-0 left-[calc(50%-400px)] right-[calc(50%-400px)] top-0 z-[100] bg-[linear-gradient(90deg,#000_10%,rgba(0,0,0,.1)49%,rgba(0,0,0,.1)51%,#000_90%)]" />
-          <div className="absolute inset-0 z-0 flex flex-col items-center justify-center overflow-hidden bg-black">
-            {data.hvtID && (
-              <video
-                className="h-full max-h-full w-full max-w-[800px] object-cover object-center"
-                playsInline
-                loop
-                autoPlay
-                disablePictureInPicture
-                ref={videoRef}
-                poster={getPosterPath(data.hvtID)}
-              >
-                <source src={getVideoPath(data.hvtID)} />
-              </video>
-            )}
-          </div>
-          <div className="relative z-[200] flex h-full flex-col justify-end bg-[rgba(0,0,0,.3)] px-[30px] pb-[30px] text-white drop-shadow-[0_0_9px_#000]">
-            <div className="absolute right-3 top-3 flex items-center gap-3">
-              <button
-                className={twMerge(
-                  "scale-100 rounded p-3 transition-[transform,colors] hover:scale-110",
-                  data.hvtID ? "bg-green-500" : "bg-neutral-500",
-                )}
-                onClick={() => {
-                  const newHvtID =
-                    window.prompt(
-                      "This should show metadata modal. hvtID:",
-                      data.hvtID,
-                    ) ?? "";
-                  setData((prev) => ({ ...prev, hvtID: newHvtID }));
-                }}
-                type="button"
-              >
-                <FiFilm />
-              </button>
-            </div>
-            <p className="sans-title-ff my-[1em] text-[13px] uppercase tracking-[.5px]">
-              introduction to the testimony of
-            </p>
-            <input
-              type="textarea"
-              name="Title"
-              placeholder="Title"
-              className="mb-[0.75em] h-[56px] w-full rounded border border-neutral-500 bg-transparent p-0 text-[56px] focus:border-white focus:ring-0"
-            />
-            <div>
-              <div className="text-[24px]">
-                <div className="flex items-center gap-1">
-                  by
-                  <input
-                    type="text"
-                    name="Author"
-                    placeholder="Author"
-                    className="h-[45px] flex-1 rounded border-neutral-500 bg-transparent p-0 text-[24px] focus:border-white focus:ring-0"
-                  />
-                </div>
-                <div className="font-helvetica text-[13px]">
-                  <input
-                    type="text"
-                    name="Affiliation"
-                    placeholder="Affiliation"
-                    className="h-[13px] w-full rounded border border-neutral-500 bg-transparent p-0 text-[13px] focus:border-white focus:ring-0"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="h-80 bg-white" />
-        </header>
-
-        <EssayPreamble hvtID={"test"} aviaryLink={"test"} />
-
-        <div
-          // style={{ top: Math.max(0, 200 - this.state.scrollPosition) }}
-          className={styles.ContentBodyContainer}
-        >
-          <main className={styles.ContentBodyContents}>
-            <Editor />
-          </main>
-
-          {/*
-          {callToAction && essayAviaryLink ? (
-            <div className={styles.CallToActionArea}>
-              <CallToAction posterURL={essayPosterPath} essay={essay} />
-            </div>
-          ) : null}
-
-          <Footer
-            orgName={config.projectData.organizationName || ""}
-            orgURL={config.projectData.homeLink || ""}
-            parentOrgName={config.projectData.parentOrganizationName || ""}
-            parentOrgURL={config.projectData.parentOrganizationURL || ""}
-          />
-          */}
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
