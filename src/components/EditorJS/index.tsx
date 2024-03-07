b3c536a6340697592d779507ebb22660f4673d77
futshi <yan.development@outlook.com>
2024-03-07 10:56:22 +0100

    download ;)

--- src/components/EditorJS/index.tsx
+++ src/components/EditorJS/index.tsx
@@ -4,6 +4,8 @@ import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 
 import dummyData from "../../../public/data/intro-hvt-0170.json" assert { type: "json" };
+import { exportToJson } from "~/utils/files";
+import { FiDownload } from "react-icons/fi";
 
 const Editorjs = dynamic(() => import("~/components/EditorJS/_editor"), {
   ssr: false,
@@ -12,15 +14,21 @@ const Editorjs = dynamic(() => import("~/components/EditorJS/_editor"), {
 export default function Editor() {
   const [editorData, setEditorData] = useState(dummyData);
 
-  useEffect(() => {
-    console.log("editorJS data:", editorData);
-  }, [editorData]);
-
   return (
-    <Editorjs
-      data={editorData}
-      onChange={setEditorData}
-      holder="editorjs-container"
-    />
+    <>
+      <Editorjs
+        data={editorData}
+        onChange={setEditorData}
+        holder="editorjs-container"
+      />
+      <button
+        className="fixed bottom-3 right-3 flex items-center gap-3 rounded bg-orange-500 p-3 transition-colors hover:bg-orange-400"
+        type="button"
+        onPointerDown={(e) => exportToJson(e, editorData)}
+      >
+        <FiDownload />
+        Download
+      </button>
+    </>
   );
 }
