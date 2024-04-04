11fc5f4613c1f3a54510388a82f4f8c690d670da
Futshi <yan.development@outlook.com>
2024-04-04 12:12:13 +0200

    static adjustments

--- src/app/essay/page.tsx
+++ src/app/essay/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, Suspense } from "react";
 import { FiArrowLeft, FiSettings } from "react-icons/fi";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -28,7 +28,7 @@ function getVideoPath(hvtID: string) {
 
 let initialized = false;
 
-export default function EssayPage() {
+function Essay() {
   const [data, setData] = useState<Essay>();
   const [showMetaModal, setShowMetaModal] = useState<boolean>(false);
 
@@ -57,7 +57,7 @@ export default function EssayPage() {
       setData(essay);
       initialized = true;
     } else {
-      alert(`Essay ${id as string} not found, redirecting...`);
+      alert(`Essay ${id ?? ""} not found, redirecting...`);
       return router.push("/");
     }
   }, [id]);
@@ -73,7 +73,7 @@ export default function EssayPage() {
     }
   }, [data]);
 
-  if (!data) return;
+  if (!data) return null;
 
   const editEssay = (meta: EssayMeta) => {
     if (!config) {
@@ -105,7 +105,7 @@ export default function EssayPage() {
     setEssays(newEssays);
     // update
     if (meta.slug !== id) {
-      window.history.pushState(null, "", `/essay/${meta.slug}`);
+      window.history.pushState(null, "", `/essay?id=${meta.slug}`);
     }
     setData((prev) => ({ ...prev!, meta }));
     setShowMetaModal(false);
@@ -113,133 +113,126 @@ export default function EssayPage() {
 
   return (
     <>
-      <div className="serif-copy-ff relative flex h-screen flex-col overflow-hidden">
-        <div className="z-[100] h-[60px] overflow-hidden shadow-[0_0_10px_rgba(0,0,0,.3)]">
-          <LogoBar />
-        </div>
+      <div className="flex-shrink flex-grow-0 basis-full overflow-scroll">
+        <header className="relative z-[40] box-border h-[50vh] overflow-hidden">
+          <div className="absolute bottom-0 left-[calc(50%-400px)] right-[calc(50%-400px)] top-0 z-[100] bg-[linear-gradient(90deg,#000_10%,rgba(0,0,0,.1)49%,rgba(0,0,0,.1)51%,#000_90%)]" />
+          <div className="absolute inset-0 z-0 flex flex-col items-center justify-center overflow-hidden bg-black">
+            {data.meta.hvtID && (
+              <video
+                className="h-full max-h-full w-full max-w-[800px] object-cover object-center"
+                playsInline
+                loop
+                autoPlay
+                disablePictureInPicture
+                ref={videoRef}
+                poster={getPosterPath(data.meta.hvtID)}
+              >
+                <source src={getVideoPath(data.meta.hvtID)} />
+              </video>
+            )}
+          </div>
+          <div className="relative z-[200] flex h-full flex-col justify-end bg-[rgba(0,0,0,.3)] px-[30px] pb-[30px] text-white">
+            <p className="sans-title-ff my-[1em] text-[13px] uppercase tracking-[.5px] [text-shadow:_0_0_9px_#000]">
+              introduction to the testimony of
+            </p>
 
-        <div className="flex-shrink flex-grow-0 basis-full overflow-scroll">
-          <header className="relative z-[40] box-border h-[50vh] overflow-hidden">
-            <div className="absolute bottom-0 left-[calc(50%-400px)] right-[calc(50%-400px)] top-0 z-[100] bg-[linear-gradient(90deg,#000_10%,rgba(0,0,0,.1)49%,rgba(0,0,0,.1)51%,#000_90%)]" />
-            <div className="absolute inset-0 z-0 flex flex-col items-center justify-center overflow-hidden bg-black">
-              {data.meta.hvtID && (
-                <video
-                  className="h-full max-h-full w-full max-w-[800px] object-cover object-center"
-                  playsInline
-                  loop
-                  autoPlay
-                  disablePictureInPicture
-                  ref={videoRef}
-                  poster={getPosterPath(data.meta.hvtID)}
-                >
-                  <source src={getVideoPath(data.meta.hvtID)} />
-                </video>
-              )}
-            </div>
-            <div className="relative z-[200] flex h-full flex-col justify-end bg-[rgba(0,0,0,.3)] px-[30px] pb-[30px] text-white">
-              <p className="sans-title-ff my-[1em] text-[13px] uppercase tracking-[.5px] [text-shadow:_0_0_9px_#000]">
-                introduction to the testimony of
-              </p>
-
-              {/* TODO:
-               * Text area is vertically scrollable, can we remove that?
-               * Has no line wrap on overflow. Add so that it behaves like the critical-edition-viewer
-               */}
+            {/* TODO:
+             * Text area is vertically scrollable, can we remove that?
+             * Has no line wrap on overflow. Add so that it behaves like the critical-edition-viewer
+             */}
+            <input
+              type="textarea"
+              name="Title"
+              placeholder="Title"
+              className="mb-[0.75em] h-[56px] w-full rounded bg-transparent p-0 text-[56px] [text-shadow:_0_0_9px_#000] focus:border-white focus:ring-0"
+              value={data.meta.title ?? ""}
+              onChange={(e) => {
+                setData((prev) => ({
+                  ...prev!,
+                  meta: { ...prev!.meta, title: e.target.value },
+                }));
+              }}
+            />
+            <div className="flex items-center text-[24px] [text-shadow:_0_0_9px_#000]">
+              by&nbsp;
               <input
                 type="textarea"
-                name="Title"
-                placeholder="Title"
-                className="mb-[0.75em] h-[56px] w-full rounded bg-transparent p-0 text-[56px] [text-shadow:_0_0_9px_#000] focus:border-white focus:ring-0"
-                value={data.meta.title ?? ""}
+                name="Author"
+                placeholder="Author"
+                className="h-[31px] flex-1 rounded bg-transparent p-0 text-[24px] [text-shadow:_0_0_9px_#000] focus:border-white focus:ring-0"
+                value={data.meta.author ?? ""}
                 onChange={(e) => {
                   setData((prev) => ({
                     ...prev!,
-                    meta: { ...prev!.meta, title: e.target.value },
+                    meta: { ...prev!.meta, author: e.target.value },
                   }));
                 }}
               />
-              <div className="flex items-center text-[24px] [text-shadow:_0_0_9px_#000]">
-                by&nbsp;
-                <input
-                  type="textarea"
-                  name="Author"
-                  placeholder="Author"
-                  className="h-[31px] flex-1 rounded bg-transparent p-0 text-[24px] [text-shadow:_0_0_9px_#000] focus:border-white focus:ring-0"
-                  value={data.meta.author ?? ""}
-                  onChange={(e) => {
-                    setData((prev) => ({
-                      ...prev!,
-                      meta: { ...prev!.meta, author: e.target.value },
-                    }));
-                  }}
-                />
-              </div>
-              <div className="font-helvetica text-[13px]">
-                <input
-                  type="textarea"
-                  name="Affiliation"
-                  placeholder="Affiliation"
-                  className="h-[13px] w-full rounded bg-transparent p-0 text-[13px] [text-shadow:_0_0_9px_#000] focus:border-white focus:ring-0"
-                  value={data.meta.affiliation ?? ""}
-                  onChange={(e) => {
-                    setData((prev) => ({
-                      ...prev!,
-                      meta: { ...prev!.meta, affiliation: e.target.value },
-                    }));
-                  }}
-                />
-              </div>
             </div>
-            <div className="h-80 bg-white" />
-          </header>
-
-          <EssayPreamble hvtID={data.meta.hvtID} />
-
-          <div
-            // style={{ top: Math.max(0, 200 - this.state.scrollPosition) }}
-            className={styles.ContentBodyContainer}
-          >
-            <main className={styles.ContentBodyContents}>
-              <Editor
-                data={data.blocks}
-                onDataChange={(data) =>
-                  setData((prev) => ({ ...prev!, blocks: data }))
-                }
+            <div className="font-helvetica text-[13px]">
+              <input
+                type="textarea"
+                name="Affiliation"
+                placeholder="Affiliation"
+                className="h-[13px] w-full rounded bg-transparent p-0 text-[13px] [text-shadow:_0_0_9px_#000] focus:border-white focus:ring-0"
+                value={data.meta.affiliation ?? ""}
+                onChange={(e) => {
+                  setData((prev) => ({
+                    ...prev!,
+                    meta: { ...prev!.meta, affiliation: e.target.value },
+                  }));
+                }}
               />
-            </main>
+            </div>
+          </div>
+          <div className="h-80 bg-white" />
+        </header>
 
-            <div className="pointer-events-none fixed bottom-5 left-5 right-5 z-10">
-              <div className="flex justify-center">
-                <div className="flex w-full max-w-7xl justify-between">
-                  <div className="flex items-center divide-x divide-white overflow-hidden rounded">
-                    <button
-                      className="pointer-events-auto flex items-center gap-3 bg-critical-600 p-3 font-[Helvetica,Arial,sans-serif] text-white transition-colors hover:bg-critical-700"
-                      type="button"
-                      onPointerDown={(e) => {
-                        if (
-                          window.confirm("Are you sure you want to go back?")
-                        ) {
-                          router.push("/");
-                        }
-                      }}
-                    >
-                      <FiArrowLeft />
-                      Back
-                    </button>
-                  </div>
+        <EssayPreamble hvtID={data.meta.hvtID} />
 
-                  <div className="flex items-center divide-x divide-white overflow-hidden rounded">
-                    <button
-                      data-modal-target="metadata-modal"
-                      data-modal-toggle="metadata-modal"
-                      className=" pointer-events-auto flex items-center gap-3 bg-critical-600 p-3 font-[Helvetica,Arial,sans-serif] text-white transition-colors hover:bg-critical-700"
-                      onClick={() => setShowMetaModal(true)}
-                      type="button"
-                    >
-                      <FiSettings /> Setttings
-                    </button>
+        <div
+          // style={{ top: Math.max(0, 200 - this.state.scrollPosition) }}
+          className={styles.ContentBodyContainer}
+        >
+          <main className={styles.ContentBodyContents}>
+            <Editor
+              data={data.blocks}
+              onDataChange={(data) =>
+                setData((prev) => ({ ...prev!, blocks: data }))
+              }
+            />
+          </main>
 
-                    {/* <button
+          <div className="pointer-events-none fixed bottom-5 left-5 right-5 z-10">
+            <div className="flex justify-center">
+              <div className="flex w-full max-w-7xl justify-between">
+                <div className="flex items-center divide-x divide-white overflow-hidden rounded">
+                  <button
+                    className="pointer-events-auto flex items-center gap-3 bg-critical-600 p-3 font-[Helvetica,Arial,sans-serif] text-white transition-colors hover:bg-critical-700"
+                    type="button"
+                    onPointerDown={(e) => {
+                      if (window.confirm("Are you sure you want to go back?")) {
+                        router.push("/");
+                      }
+                    }}
+                  >
+                    <FiArrowLeft />
+                    Back
+                  </button>
+                </div>
+
+                <div className="flex items-center divide-x divide-white overflow-hidden rounded">
+                  <button
+                    data-modal-target="metadata-modal"
+                    data-modal-toggle="metadata-modal"
+                    className=" pointer-events-auto flex items-center gap-3 bg-critical-600 p-3 font-[Helvetica,Arial,sans-serif] text-white transition-colors hover:bg-critical-700"
+                    onClick={() => setShowMetaModal(true)}
+                    type="button"
+                  >
+                    <FiSettings /> Setttings
+                  </button>
+
+                  {/* <button
                       className="flex items-center gap-3 bg-critical-600 p-3 font-[Helvetica,Arial,sans-serif] text-white transition-colors hover:bg-critical-700"
                       type="button"
                       onPointerDown={
@@ -250,7 +243,6 @@ export default function EssayPage() {
                       <FiDownload />
                       JSON
                     </button> */}
-                  </div>
                 </div>
               </div>
             </div>
@@ -265,5 +257,19 @@ export default function EssayPage() {
         onSave={(meta) => editEssay(meta)}
       />
     </>
+  );
+}
+
+export default function EssayPage() {
+  return (
+    <div className="serif-copy-ff relative flex h-screen flex-col overflow-hidden">
+      <div className="z-[100] h-[60px] overflow-hidden shadow-[0_0_10px_rgba(0,0,0,.3)]">
+        <LogoBar />
+      </div>
+
+      <Suspense>
+        <Essay />
+      </Suspense>
+    </div>
   );
 }
