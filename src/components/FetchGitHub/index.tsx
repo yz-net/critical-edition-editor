644a94ae2a1df8c0fd1fae89ddc6a8acbe6a89f0
Futshi <yan.development@outlook.com>
2024-07-26 15:53:13 +0200

    fetchGitHub essays.meta.id added

--- src/components/FetchGitHub/index.tsx
+++ src/components/FetchGitHub/index.tsx
@@ -10,8 +10,12 @@ const confirmText =
   "Are you sure you want to pull content from GitHub? All local changes will be deleted.";
 
 export default function Import() {
-  const localDataStore = useLocalDataStore();
-  const gitDataStore = useGitDataStore();
+  const localSetConfig = useLocalDataStore((state) => state.setConfig);
+  const localSetEssays = useLocalDataStore((state) => state.setEssays);
+
+  const gitSetConfig = useGitDataStore((state) => state.setConfig);
+  const gitSetEssays = useGitDataStore((state) => state.setEssays);
+
   const setLoading = useStateStore((state) => state.setLoading);
   const setToast = useStateStore((state) => state.setToast);
 
@@ -20,10 +24,15 @@ export default function Import() {
       setLoading(true);
       try {
         const data = await fetchGitHubData();
-        gitDataStore.setConfig(data.config);
-        gitDataStore.setEssays(data.essays);
-        localDataStore.setConfig(data.config);
-        localDataStore.setEssays(data.essays);
+        data.essays = data.essays.map((e) => ({
+          ...e,
+          meta: { ...e.meta, id: e.meta.slug },
+        }));
+        gitSetConfig(data.config);
+        gitSetEssays(data.essays);
+        localSetConfig(data.config);
+        localSetEssays(data.essays);
+
         setToast({
           className: "bg-yellow-300 text-black",
           text: "GitHub data fetched successfully",
